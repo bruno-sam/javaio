08d5764fd0174d5699b8cbe48b5de797b3924b18
bruno_sam <verdibianco@hotmail.com>
2019-08-26 10:35:30 -0300

    Novo arquivo Escrita

---
+++ java-io/src/br/com/alura/java/io/teste/Escrita.java
@@ -0,0 +1,41 @@
+package br.com.alura.java.io.teste;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Escrita {
+	
+	public static void main(String[] args) throws IOException {
+		
+		File file = new File("c:\\Temp\\Escrita.txt");
+		
+		if(!file.exists()){
+			  BufferedWriter writer = new BufferedWriter(new FileWriter("c:\\Temp\\escrita.txt",true));
+			 System.out.println("Arquivo criado com sucesso");
+						
+						int i = 0;
+						while(i < 20){
+							
+							  writer.append("Bruno");
+							  writer.newLine();
+							i++;
+						}
+			
+		    writer.close();
+		}else{
+			System.out.println("Arquivo ja existe");
+		}
+		
+		
+	
+		
+	
+	
+		
+		
+		
+	}
+}
